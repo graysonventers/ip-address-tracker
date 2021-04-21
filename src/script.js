5fc60df42dc5ab7dd95382b6d9d8ba972cd01bb6
Grayson Venters <graysonventers@gmail.com>
2021-04-21 13:59:01 -0700

    basic functionality of leaflet map loading and IP geolcation api call completed. Removed webpack for simplicty of project

--- src/script.js
+++ src/script.js
@@ -1,23 +1,51 @@
-// require('dotenv').config();
+// DOM Elements
+const ipAddressSpan = document.getElementById('ipAddress');
+const locationSpan = document.getElementById('location');
+const timezoneSpan = document.getElementById('timezone');
+const ispSpan = document.getElementById('isp');
 
-alert('connected')
 
-// console.log(process.env)
+let ipAAddress = '';
+let domainAddress = 'ebay.com';
+const api_key = 'at_hlldI2XUhNd2GIJ4XptHrsXlIZEga';
+
 // import map
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+function loadMap(lat, long) {
+    var mymap = L.map('mapid').setView([lat, long], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    }).addTo(mymap);
+}
+
+// add information to informationCard
+function loadInformationCardData (ip, location, timezone, isp) {
+    ipAddressSpan.innerHTML=ip;
+    locationSpan.innerHTML=location;
+    timezoneSpan.innerHTML=timezone;
+    ispSpan.innerHTML=isp;
+}
+
 
 // call geolocationAPI
 async function getGeolocation() {
     try {
-        const res = await fetch(`https://geo.ipify.org/api/v1?apiKey=at_hlldI2XUhNd2GIJ4XptHrsXlIZEga&ipAddress=8.8.8.8
-        `)
-
+        const res = await fetch(`https://geo.ipify.org/api/v1?apiKey=${api_key}&ipAddress=${ipAAddress}&domain=${domainAddress}`);
         const data = await res.json();
         console.log(data)
+        // variables for loadMap
+        const latitude = data.location.lat;
+        const longitude = data.location.lng;
+
+        // variables for loadInformationCardData
+        const ipAddress = data.ip;
+        const location = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`
+        const timezone = `UTC ${data.location.timezone}`
+        const isp = data.isp;
+
+        loadInformationCardData(ipAddress, location, timezone, isp);        
+        loadMap(latitude, longitude);
+
     } catch (error) {
         console.error(error)
     }
