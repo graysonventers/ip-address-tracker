8c45346e34ea08488160a669bf2bde05768c8c33
Grayson Venters <graysonventers@gmail.com>
2021-04-26 11:20:56 -0700

    moved myMap initializer outside of loadMap function which fixed issue of map not changing views on updated lat and long

--- src/script.js
+++ src/script.js
@@ -3,19 +3,75 @@ const ipAddressSpan = document.getElementById('ipAddress');
 const locationSpan = document.getElementById('location');
 const timezoneSpan = document.getElementById('timezone');
 const ispSpan = document.getElementById('isp');
+let inputString = document.getElementById('ipDomainInput');
+const submitButton = document.getElementById('submitButton');
 
 
-let ipAAddress = '';
-let domainAddress = 'ebay.com';
+// would normally hide in config file
 const api_key = 'at_hlldI2XUhNd2GIJ4XptHrsXlIZEga';
 
-// import map
-function loadMap(lat, long) {
-    var mymap = L.map('mapid').setView([lat, long], 13);
+let ipAddressInput = '';
+let domainAddress = '';
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// initialize map container
+let myMap = L.map('mapid');
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(mymap);
+    })
+    .addTo(myMap);
+
+const onClickButton = () => {
+    // check if incoming search is IP address or domain name
+    const ipFormat = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+
+    if (inputString.value.match(ipFormat)) {
+        ipAddressInput = inputString.value;
+    } else {
+        domainAddress = inputString.value;
+    }
+    getGeolocation();
+}
+
+// import map
+function initialLoadMap(lat, long) {
+    
+    // set view on map with lat and long passed in from getGeolocation
+    myMap.setView([lat, long], 13);
+
+    const myIcon = L.icon({
+        iconUrl: '../images/icon-location.svg',
+        // iconSize: [30, 40]
+    })
+    L.marker([lat, long], {icon: myIcon}).addTo(myMap);
+}
+
+// call geolocationAPI
+async function getGeolocation() {
+    try {
+        const res = await fetch(`https://geo.ipify.org/api/v1?apiKey=${api_key}&ipAddress=${ipAddressInput}&domain=${domainAddress}`, {
+            method: 'GET'
+        });
+        const data = await res.json();
+    
+        // variables for loadMap
+        let latitude = data.location.lat;
+        let longitude = data.location.lng;
+
+        // variables for loadInformationCardData
+        const ipAddress = data.ip;
+        const location = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`
+        const timezone = `UTC ${data.location.timezone}`
+        const isp = data.isp;
+
+        
+        initialLoadMap(latitude, longitude);
+        loadInformationCardData(ipAddress, location, timezone, isp);        
+        
+
+    } catch (error) {
+        console.log(error.message)
+        // alert('IP address or domain is not valid')
+    }
 }
 
 // add information to informationCard
@@ -26,30 +82,5 @@ function loadInformationCardData (ip, location, timezone, isp) {
     ispSpan.innerHTML=isp;
 }
 
-
-// call geolocationAPI
-async function getGeolocation() {
-    try {
-        const res = await fetch(`https://geo.ipify.org/api/v1?apiKey=${api_key}&ipAddress=${ipAAddress}&domain=${domainAddress}`);
-        const data = await res.json();
-        console.log(data)
-        // variables for loadMap
-        const latitude = data.location.lat;
-        const longitude = data.location.lng;
-
-        // variables for loadInformationCardData
-        const ipAddress = data.ip;
-        const location = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`
-        const timezone = `UTC ${data.location.timezone}`
-        const isp = data.isp;
-
-        loadInformationCardData(ipAddress, location, timezone, isp);        
-        loadMap(latitude, longitude);
-
-    } catch (error) {
-        console.error(error)
-    }
-    
-}
-
-getGeolocation()
+window.addEventListener('load', getGeolocation);
+submitButton.addEventListener('click', onClickButton);
