0faa8630b146eda66a9b1817d54133130a707dee
Grayson Venters <graysonventers@gmail.com>
2021-05-19 11:40:54 -0700

    removed example lodash function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-import _ from 'lodash'
-
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-}
-  
-document.body.appendChild(component());
 
 // DOM Elements
 const ipAddressSpan = document.getElementById('ipAddress');
